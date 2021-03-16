09fb28d05b770f66c3c1b4a6cd4e3b63fc34c7d2
Athman Bakari <disguisedsandwich@gmail.com>
2021-03-16 18:55:12 +0300

    sending pdf invoices on email asynchronously

--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 import braintree
 from django.conf import settings
 from orders.models import Order
+from .tasks import payment_completed
 
 #instantiate Braintree payment gateway
 gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
@@ -24,6 +25,8 @@ def payment_process(request):
             #store the unique transaction id 
             order.braintree_id = result.transaction.id
             order.save()
+            #launch asychronous task
+            payment_completed.delay(order.id)
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
