fb526f8f772326529896e43368d752a33a84b445
Athman Bakari <disguisedsandwich@gmail.com>
2021-03-17 01:47:54 +0300

    Added a coupon system

--- parrot/settings.py
+++ parrot/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
-    'payment.apps.PaymentConfig'
+    'payment.apps.PaymentConfig',
+    'coupons.apps.CouponsConfig'
 ]
 
 MIDDLEWARE = [
