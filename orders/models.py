fad835d752d86d0dcef264f4f3fda4130129cb0c
Athman Bakari <disguisedsandwich@gmail.com>
2021-03-19 04:17:58 +0300

    Added swahili, spanish translations

--- orders/models.py
+++ orders/models.py
@@ -3,14 +3,15 @@ from shop.models import Product
 from decimal import Decimal
 from django.core.validators import MinValueValidator, MaxValueValidator
 from coupons.models import Coupon
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    table_number = models.CharField(max_length=100)
+    first_name = models.CharField(_('first_name'), max_length=50)
+    last_name = models.CharField(_('last_name'), max_length=50)
+    email = models.EmailField(_('email'))
+    address = models.CharField(_('address'),max_length=250)
+    table_number = models.CharField(_('table_number'),max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now = True)
     paid = models.BooleanField(default=False)
--- parrot/settings.py
+++ parrot/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import braintree
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,12 +44,14 @@ INSTALLED_APPS = [
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
-    'coupons.apps.CouponsConfig'
+    'coupons.apps.CouponsConfig',
+    'rosetta'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -112,8 +115,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'en'
+LANGUAGES = (
+        ('en', _('English')),
+        ('sw', _('Kiswahili')),
+        ('es', _('Spanish'))
+        )
+LOCALE_PATHS =(
+        os.path.join(BASE_DIR, 'locale/'),
+        )
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
